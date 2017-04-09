478ec609d4d1852d88e35c3fcfa069aefa966edf
sai3010 <saiprasanth2007@gmail.com>
2017-04-09 18:33:54 +0530

    just snowman

--- CGV Proj/snowman.cpp
+++ CGV Proj/snowman.cpp
@@ -12,20 +12,6 @@
 // escape key (for exit)
 #define ESC 27
 
-//----------------------------------------------------------------------
-// Global variables
-//
-// The coordinate system is set up so that the (x,y)-coordinate plane
-// is the ground, and the z-axis is directed upwards. The y-axis points
-// to the north and the x-axis points to the east.
-//
-// The values (x,y) are the current camera position. The values (lx, ly)
-// point in the direction the camera is looking. The variables angle and
-// deltaAngle control the camera's angle. The variable deltaMove
-// indicates the amount of incremental motion for the camera with each
-// redraw cycle. The variables isDragging and xDragStart are used to
-// monitor the mouse when it drags (with the left button down).
-//----------------------------------------------------------------------
 
 // Camera position
 float x = 0.0, y = -5.0; // initially 5 units south of origin
@@ -40,15 +26,6 @@ float deltaAngle = 0.0; // additional angle change when dragging
 int isDragging = 0; // true when dragging
 int xDragStart = 0; // records the x-coordinate when dragging starts
 
-					//----------------------------------------------------------------------
-					// Reshape callback
-					//
-					// Window size has been set/changed to w by h pixels. Set the camera
-					// perspective to 45 degree vertical field of view, a window aspect
-					// ratio of w/h, a near clipping plane at depth 1, and a far clipping
-					// plane at depth 100. The viewport is the entire window.
-					//
-					//----------------------------------------------------------------------
 void changeSize(int w, int h)
 {
 	float ratio = ((float)w) / ((float)h); // window aspect ratio
@@ -59,20 +36,8 @@ void changeSize(int w, int h)
 	glViewport(0, 0, w, h); // set viewport (drawing area) to entire window
 }
 
-//----------------------------------------------------------------------
-// Draw one snowmen (at the origin)
-//
-// A snowman consists of a large body sphere and a smaller head sphere.
-// The head sphere has two black eyes and an orange conical nose. To
-// better create the impression they are sitting on the ground, we draw
-// a fake shadow, consisting of a dark circle under each.
-//
-// We make extensive use of nested transformations. Everything is drawn
-// relative to the origin. The snowman's eyes and nose are positioned
-// relative to a head sphere centered at the origin. Then the head is
-// translated into its final position. The body is drawn and translated
-// into its final position.
-//----------------------------------------------------------------------
+
+
 void drawSnowman()
 {
 	// Draw body (a 20x20 spherical mesh of radius 0.75 at height 0.75)
@@ -112,33 +77,12 @@ void drawSnowman()
 	// Draw a faux shadow beneath snow man (dark green circle)
 	glColor3f(0.0, 0.5, 0.0);
 	glPushMatrix();
-	glTranslatef(0.2, 0.2, 0.001);	// translate to just above ground
+	glTranslatef(0.5, 0.5, 0.001);	// translate to just above ground
 	glScalef(1.0, 1.0, 0.0); // scale sphere into a flat pancake
 	glutSolidSphere(0.75, 20, 20); // shadow same size as body
 	glPopMatrix();
 }
 
-//----------------------------------------------------------------------
-// Update with each idle event
-//
-// This incrementally moves the camera and requests that the scene be
-// redrawn.
-//----------------------------------------------------------------------
-void update(void)
-{
-	if (deltaMove) { // update camera position
-		x += deltaMove * lx * 0.1;
-		y += deltaMove * ly * 0.1;
-	}
-	glutPostRedisplay(); // redisplay everything
-}
-
-//----------------------------------------------------------------------
-// Draw the entire scene
-//
-// We first update the camera location based on its distance from the
-// origin and its direction.
-//----------------------------------------------------------------------
 void renderScene(void)
 {
 	int i, j;
@@ -153,7 +97,7 @@ void renderScene(void)
 	// Set the camera centered at (x,y,1) and looking along directional
 	// vector (lx, ly, 0), with the z-axis pointing up
 	gluLookAt(
-		x, y, 1.0,
+		x, y, 1,
 		x + lx, y + ly, 1.0,
 		0.0, 0.0, 1.0);
 
@@ -178,105 +122,31 @@ void renderScene(void)
 	glutSwapBuffers(); // Make it all visible
 }
 
-//----------------------------------------------------------------------
-// User-input callbacks
-//
-// processNormalKeys: ESC, q, and Q cause program to exit
-// pressSpecialKey: Up arrow = forward motion, down arrow = backwards
-// releaseSpecialKey: Set incremental motion to zero
-//----------------------------------------------------------------------
-void processNormalKeys(unsigned char key, int xx, int yy)
-{
-	if (key == ESC || key == 'q' || key == 'Q') exit(0);
-}
 
-void pressSpecialKey(int key, int xx, int yy)
-{
-	switch (key) {
-	case GLUT_KEY_UP: deltaMove = 1.0; break;
-	case GLUT_KEY_DOWN: deltaMove = -1.0; break;
-	}
-}
 
-void releaseSpecialKey(int key, int x, int y)
-{
-	switch (key) {
-	case GLUT_KEY_UP: deltaMove = 0.0; break;
-	case GLUT_KEY_DOWN: deltaMove = 0.0; break;
-	}
-}
 
-//----------------------------------------------------------------------
-// Process mouse drag events
-// 
-// This is called when dragging motion occurs. The variable
-// angle stores the camera angle at the instance when dragging
-// started, and deltaAngle is a additional angle based on the
-// mouse movement since dragging started.
-//----------------------------------------------------------------------
-void mouseMove(int x, int y)
-{
-	if (isDragging) { // only when dragging
-					  // update the change in angle
-		deltaAngle = (x - xDragStart) * 0.005;
-
-		// camera's direction is set to angle + deltaAngle
-		lx = -sin(angle + deltaAngle);
-		ly = cos(angle + deltaAngle);
-	}
-}
-
-void mouseButton(int button, int state, int x, int y)
-{
-	if (button == GLUT_LEFT_BUTTON) {
-		if (state == GLUT_DOWN) { // left mouse button pressed
-			isDragging = 1; // start dragging
-			xDragStart = x; // save x where button first pressed
-		}
-		else { /* (state = GLUT_UP) */
-			angle += deltaAngle; // update camera turning angle
-			isDragging = 0; // no longer dragging
-		}
-	}
-}
 
 //----------------------------------------------------------------------
 // Main program  - standard GLUT initializations and callbacks
 //----------------------------------------------------------------------
 int main(int argc, char **argv)
 {
-	printf("\n\
------------------------------------------------------------------------\n\
-  OpenGL Sample Program:\n\
-  - Drag mouse left-right to rotate camera\n\
-  - Hold up-arrow/down-arrow to move camera forward/backward\n\
-  - q or ESC to quit\n\
------------------------------------------------------------------------\n");
+
 
 	// general initializations
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(800, 400);
-	glutCreateWindow("OpenGL/GLUT Sampe Program");
+	glutCreateWindow("snowman");
 
 	// register callbacks
-	glutReshapeFunc(changeSize); // window reshape callback
 	glutDisplayFunc(renderScene); // (re)display callback
-	glutIdleFunc(update); // incremental update 
-	glutIgnoreKeyRepeat(1); // ignore key repeat when holding key down
-	glutMouseFunc(mouseButton); // process mouse button push/release
-	glutMotionFunc(mouseMove); // process mouse dragging motion
-	glutKeyboardFunc(processNormalKeys); // process standard key clicks
-	glutSpecialFunc(pressSpecialKey); // process special key pressed
-									  // Warning: Nonstandard function! Delete if desired.
-	glutSpecialUpFunc(releaseSpecialKey); // process special key release
-
-										  // OpenGL init
+	glutReshapeFunc(changeSize); // window reshape callback
 	glEnable(GL_DEPTH_TEST);
 
 	// enter GLUT event processing cycle
 	glutMainLoop();
 
-	return 0; // this is just to keep the compiler happy
+	return 0;
 }
